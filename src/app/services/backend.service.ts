a2672435b1c03223c2eab8bf982dd0efaff2bddf
tomasz.dobosz <tomasz.dobosz@tiskel.com>
2025-03-19 20:24:15 +0100

    env

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
 
-  private apiUrl = "http://localhost:3535";
+  private apiUrl = environment.apiUrl;
 
   constructor(
     private httpClient: HttpClient
